5dbbc722c1aeccbb87422e07a5436509c35919ac
Aida <88146345+Aida010183@users.noreply.github.com>
2022-01-29 17:45:19 +0300

    Add files via upload

---
+++ task/script1.js
@@ -0,0 +1,4 @@
+// 1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере.
+let userName = "I will always do it, I need to see the results.";
+alert(userName);
+document.write(userName);
