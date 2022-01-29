f52a73cd192d155dd0567bcf4f918d6f6b9bc123
Aida <88146345+Aida010183@users.noreply.github.com>
2022-01-29 13:39:25 +0300

    Add files via upload

---
+++ Задача1/script2.js
@@ -0,0 +1,5 @@
+// 2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение.
+let userMessage;
+userMessage = "Let's learn!";
+userMessage = "Save the world!";
+document.write(userMessage);
