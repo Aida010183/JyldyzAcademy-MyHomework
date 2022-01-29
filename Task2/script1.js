71cfbf47228ae10836245d6d27c5fe01007d59cf
Aida <88146345+Aida010183@users.noreply.github.com>
2022-01-29 13:52:22 +0300

    Add files via upload

---
+++ Task2/script1.js
@@ -0,0 +1,4 @@
+// 1. Дана строка **'ddd@bbb@ccc'**. Замените все **@** на **'!'**
+let userString = "ddd@bbb@ccc";
+alert(userString.replace(/@/g, "!")); // это не для вас, а для меня пример.
+document.write(userString.replace(/@/g, "!") + "<br>");
