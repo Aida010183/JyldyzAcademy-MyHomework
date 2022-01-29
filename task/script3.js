8eeaa9e455ac04f9bc619905919dfe4b2ca52bfe
Aida <88146345+Aida010183@users.noreply.github.com>
2022-01-29 17:49:58 +0300

    Delete task directory

--- task/script3.js
+++
@@ -1,8 +0,0 @@
-// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
-//
-let userSurName = prompt("Your last name:");
-let userName = prompt("Your first name:");
-let userEmail = prompt("Your email:");
-document.write("<h1>Your last name: " + userSurName + "</h1>");
-document.write("<h1>Your first name: " + userName + "</h1>");
-document.write("<h1>Your email: " + userEmail + "</h1>");
