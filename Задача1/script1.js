095ff47d2f59e4333f6748b99a6a72b291c5aae4
Aida <88146345+Aida010183@users.noreply.github.com>
2022-01-29 13:56:35 +0300

    Delete Задача1 directory

--- Задача1/script1.js
+++
@@ -1,3 +0,0 @@
-// 1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере.
-let userName = "I will always do it, I need to see the results.";
-document.write(userName);
